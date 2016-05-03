105c05d6a643c469eacaa0518fa23cc01bb2c1e2
dbonillao <delcybonilla91@gmail.com>
2016-05-03 20:20:06 +0200

    Red and green LEDs
    not working completely - both LEDs are on when fromTLC[0]>0.

--- Task 2/potentiometer.py
+++ Task 2/potentiometer.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
-import spidev
-import time
-# create SPI instance, open bus
 spi = spidev.SpiDev()
 spi.open(0,0)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+
 while True:
-	# get 1 byte from TLC
-	fromTLC = spi.xfer2([0x00])
-	print fromTLC[0]
-	time.sleep(0.5)
+        # get 1 byte from TLC
+        fromTLC = spi.xfer2([0x00])
+        print fromTLC[0]
+        if(fromTLC[0] == 0):
+                GPIO.output(11,GPIO.LOW)
+                GPIO.output(13,GPIO.LOW)
+        elif(fromTLC[0]>191):
+                GPIO.output(11,GPIO.HIGH)
+                GPIO.output(13,GPIO.LOW)
+        else:
+                GPIO.output(13,GPIO.HIGH)
+                GPIO.output(11,GPIO.LOW)
+        time.sleep(1)
