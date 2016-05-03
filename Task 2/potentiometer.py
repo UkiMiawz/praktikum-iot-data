44efa67ff3928c7f027662b384e264abd5d4e7ec
dbonillao <delcybonilla91@gmail.com>
2016-05-03 20:53:19 +0200

    Print percentage

--- Task 2/potentiometer.py
+++ Task 2/potentiometer.py
@@ -3,20 +3,23 @@ spi = spidev.SpiDev()
 spi.open(0,0)
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 
 while True:
         # get 1 byte from TLC
         fromTLC = spi.xfer2([0x00])
-        print fromTLC[0]
-        if(fromTLC[0] == 0):
+        per=(fromTLC[0]/255.00)*100
+        print fromTLC[0]," percentage: ","%.2f"%per,"%"
+        if(per == 0):
                 GPIO.output(11,GPIO.LOW)
                 GPIO.output(13,GPIO.LOW)
-        elif(fromTLC[0]>191):
+        elif(per>75):
                 GPIO.output(11,GPIO.HIGH)
                 GPIO.output(13,GPIO.LOW)
         else:
                 GPIO.output(13,GPIO.HIGH)
                 GPIO.output(11,GPIO.LOW)
         time.sleep(1)
+
