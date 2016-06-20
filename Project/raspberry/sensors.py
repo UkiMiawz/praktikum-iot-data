880e29249be2344770885038bfc9efc370add135
UkiMiawz <melisa.cecilia.mail@gmail.com>
2016-06-20 10:09:47 +0200

    Fix indentation

--- Project/raspberry/sensors.py
+++ Project/raspberry/sensors.py
@@ -26,66 +26,66 @@ pin = 4
 logger = app_logging.get_logger()
 
 def on_publish(mosq, obj, mid):
-    logger.info("mid: " + str(mid))
+	logger.info("mid: " + str(mid))
 
 try:
 
-    #mqtt client
-    mosquitto_client = mqtt.Client()
-    url = urlparse.urlparse("mqtt://{host}:{port}".format(host=mqtt_host, port=mqtt_port))
-    mosquitto_client.connect(url.hostname, url.port)
+	#mqtt client
+	mosquitto_client = mqtt.Client()
+	url = urlparse.urlparse("mqtt://{host}:{port}".format(host=mqtt_host, port=mqtt_port))
+	mosquitto_client.connect(url.hostname, url.port)
 
-    #keen client
-    keen_client = KeenClient(
-        project_id=keen_project_id,  # your project ID for collecting cycling data
-        write_key=keen_write_key,
-        read_key=keen_read_key,
-        master_key=keen_master_key
-    )
+	#keen client
+	keen_client = KeenClient(
+		project_id=keen_project_id,  # your project ID for collecting cycling data
+		write_key=keen_write_key,
+		read_key=keen_read_key,
+		master_key=keen_master_key
+	)
 
-    while True:
+	while True:
 
-        #generate data
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-	    tsl = tsl2591.Tsl2591()
-	    full, ir = tsl.get_full_luminosity() # read raw values (full spectrum and ir spectrum)
-	    lux = tsl.calculate_lux(full, ir) # convert raw values to lux
+		#generate data
+		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+		tsl = tsl2591.Tsl2591()
+		full, ir = tsl.get_full_luminosity() # read raw values (full spectrum and ir spectrum)
+		lux = tsl.calculate_lux(full, ir) # convert raw values to lux
 
-        if humidity is not None and temperature is not None:
-            logger.info('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-        else:
-            logger.error('Error failed to get reading from DHT11 sensor. Try again!')
-            raise
+		if humidity is not None and temperature is not None:
+			logger.info('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+		else:
+			logger.error('Error failed to get reading from DHT11 sensor. Try again!')
+			raise
 
-	    if lux is not None:
-		    logger.info('Lux={0:0.1f}lux'.format(lux))
-	    else:
-            logger.error('Error failed to get reading from TSL2591 sensor. Try again!')
-            raise
+		if lux is not None:
+			logger.info('Lux={0:0.1f}lux'.format(lux))
+		else:
+			logger.error('Error failed to get reading from TSL2591 sensor. Try again!')
+			raise
 
-        #publish to eclipse messaging
-        mosquitto_client.on_publish = on_publish
-        mosquitto_client.publish(humidity_topic, '{{"humidity": {humidity}}}'.format(humidity=humidity,))
-        mosquitto_client.publish(temperature_topic, '{{"temperature": {temperature}}}'.format(temperature=temperature,))
-        mosquitto_client.publish(lux_topic, '{{"lux": {0:0.2f}}}'.format(lux))
+		#publish to eclipse messaging
+		mosquitto_client.on_publish = on_publish
+		mosquitto_client.publish(humidity_topic, '{{"humidity": {humidity}}}'.format(humidity=humidity,))
+		mosquitto_client.publish(temperature_topic, '{{"temperature": {temperature}}}'.format(temperature=temperature,))
+		mosquitto_client.publish(lux_topic, '{{"lux": {0:0.2f}}}'.format(lux))
 
-        #separate events into each topic
-        keen_client.add_event(KEEN_HUMIDITY_TOPIC, {
-            "humidity": humidity
-        })
+		#separate events into each topic
+		keen_client.add_event(KEEN_HUMIDITY_TOPIC, {
+			"humidity": humidity
+		})
 
-        keen_client.add_event(KEEN_TEMPERATURE_TOPIC, {
-            "temperature": temperature
-        })
+		keen_client.add_event(KEEN_TEMPERATURE_TOPIC, {
+			"temperature": temperature
+		})
 
-        keen_client.add_event(KEEN_LUX_TOPIC, {
-            "lux": float('{0:0.2f}'.format(lux))
-        })
+		keen_client.add_event(KEEN_LUX_TOPIC, {
+			"lux": float('{0:0.2f}'.format(lux))
+		})
 
-        time.sleep(TIME_INTERVAL)
+		time.sleep(TIME_INTERVAL)
 
 except:
 
-    trace = traceback.format_exc()
-    logger.error("Unexpected error: %s" % trace)
-    raise
+	trace = traceback.format_exc()
+	logger.error("Unexpected error: %s" % trace)
+	raise
