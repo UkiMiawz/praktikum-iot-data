b18de22dc37582d409a6f1300c8ee25f6d3da3d4
UkiMiawz <melisa.cecilia.mail@gmail.com>
2016-06-20 10:05:58 +0200

    separate connection outside

--- Project/raspberry/sensors.py
+++ Project/raspberry/sensors.py
@@ -29,41 +29,45 @@ def on_publish(mosq, obj, mid):
     logger.info("mid: " + str(mid))
 
 try:
+
+    #mqtt client
+    mosquitto_client = mqtt.Client()
+    url = urlparse.urlparse("mqtt://{host}:{port}".format(host=mqtt_host, port=mqtt_port))
+    mosquitto_client.connect(url.hostname, url.port)
+
+    #keen client
+    keen_client = KeenClient(
+        project_id=keen_project_id,  # your project ID for collecting cycling data
+        write_key=keen_write_key,
+        read_key=keen_read_key,
+        master_key=keen_master_key
+    )
+
     while True:
+
         #generate data
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-	tsl = tsl2591.Tsl2591()
-	full, ir = tsl.get_full_luminosity() # read raw values (full spectrum and ir spectrum)
-	lux = tsl.calculate_lux(full, ir) # convert raw values to lux
-		
+	    tsl = tsl2591.Tsl2591()
+	    full, ir = tsl.get_full_luminosity() # read raw values (full spectrum and ir spectrum)
+	    lux = tsl.calculate_lux(full, ir) # convert raw values to lux
+
         if humidity is not None and temperature is not None:
             logger.info('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
         else:
             logger.error('Error failed to get reading from DHT11 sensor. Try again!')
             raise
-	if lux is not None:
-		logger.info('Lux={0:0.1f}lux'.format(lux))
-	else:
+
+	    if lux is not None:
+		    logger.info('Lux={0:0.1f}lux'.format(lux))
+	    else:
             logger.error('Error failed to get reading from TSL2591 sensor. Try again!')
             raise
-
-        mosquitto_client = mqtt.Client()
-        url = urlparse.urlparse("mqtt://{host}:{port}".format(host=mqtt_host, port=mqtt_port))
-        mosquitto_client.connect(url.hostname, url.port)
 
         #publish to eclipse messaging
         mosquitto_client.on_publish = on_publish
         mosquitto_client.publish(humidity_topic, '{{"humidity": {humidity}}}'.format(humidity=humidity,))
         mosquitto_client.publish(temperature_topic, '{{"temperature": {temperature}}}'.format(temperature=temperature,))
         mosquitto_client.publish(lux_topic, '{{"lux": {0:0.2f}}}'.format(lux))
-
-        #push to keen
-        keen_client = KeenClient(
-            project_id=keen_project_id,  # your project ID for collecting cycling data
-            write_key=keen_write_key,
-            read_key=keen_read_key,
-            master_key=keen_master_key
-        )
 
         #separate events into each topic
         keen_client.add_event(KEEN_HUMIDITY_TOPIC, {
@@ -81,6 +85,7 @@ try:
         time.sleep(TIME_INTERVAL)
 
 except:
+
     trace = traceback.format_exc()
     logger.error("Unexpected error: %s" % trace)
     raise
