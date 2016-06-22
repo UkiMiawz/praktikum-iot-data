b5206ae0c1dc92ea3bb3f0283ad76b74629bfcb0
dbonillao <delcybonilla91@gmail.com>
2016-06-22 15:27:32 +0200

    Updated Task 6

--- Task 6/mqtt_voltage.sh
+++ Task 6/mqtt_voltage.sh
@@ -2,6 +2,6 @@ while [ 1 ]
 do
 	tm=`/opt/vc/bin/vcgencmd measure_volts core`
 	volt=`echo $tm| cut -d '=' -f2`
-	mosquitto_pub -t 'groupB/voltage' -m $volt
+	mosquitto_pub -h 192.76.241.164 -t 'groupB/voltage' -m $volt
 	sleep 5s
 done
