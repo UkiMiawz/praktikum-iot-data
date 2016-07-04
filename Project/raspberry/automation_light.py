f69c52033f7a2a06b6b4ad37b9f6620df4db6465
Mr Garcia <mr@garcia.com>
2016-07-04 22:57:41 +0200

    Add automation for light

---
+++ Project/raspberry/automation_light.py
@@ -0,0 +1,16 @@
+import sys
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BOARD)
+# Pin 11 (GPIO 17) as output
+GPIO.setup(11, GPIO.OUT)
+
+msg = sys.argv[1]
+if(msg=='on'):
+	#LED on
+	GPIO.output(11, GPIO.HIGH)
+else:
+ 	#LED off
+	GPIO.output(11, GPIO.LOW)
